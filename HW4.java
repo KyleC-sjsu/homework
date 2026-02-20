6e74f3ca91027592afc6f96e7838e28ea291773c
KyleC-sjsu <kyle.chen02@sjsu.edu>
2026-02-19 22:15:03 -0800

    Start HW4

---
+++ HW4.java
@@ -0,0 +1,18 @@
+// Define Node class
+public class Node {
+    int key;
+    Node next;
+
+    // Create constructor
+    public Node (int number) {
+        this.number = number;
+        this.next = null;
+    }
+}
+
+public class HW4 {
+    public static void main(String[] args) {
+        // Do main method requirements
+        
+    }
+}
